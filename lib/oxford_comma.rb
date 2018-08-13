99785ca7b3f1d7a87f701a41adf8a9c8367deb9e
Mendy <mendybeltcohen@gmail.com>
2018-08-13 14:10:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,5 +13,7 @@ def oxford_comma(array)
      array.join(' and ') 
   elsif array.length == 3
       array[0..1].join(', ') << ', ' <<  'and ' << array[2]
+      elsif array.length > 3 
+      array.join
   end
 end
