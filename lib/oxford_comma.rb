215f40ee3be9fb6a94a056ed3d035594ae1e2e5f
Mendy <mendybeltcohen@gmail.com>
2018-08-13 13:58:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   if array.length == 0 
+#     array.join
+#   #   elsif array.length == 
+#   # array.slice("and")
+# end
+# end
 
+def oxford_comma(array)
+  if array.length == 1
+     array.join
+  elsif array.length == 2
+     array.join(' and ') 
+  elsif array.length == 3
+     array.join(', ')  [13] << ('and ')
+  end
 end
