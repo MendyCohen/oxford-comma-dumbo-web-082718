592c4a62593423d158b3c1aca23968c0f8a11377
Mendy <mendybeltcohen@gmail.com>
2018-08-13 15:21:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,7 +15,7 @@ def oxford_comma(array)
       array[0..1].join(', ') << ', ' <<  'and ' << array[2]
       elsif array.length > 3 
       #array.join
-      array.concat(['and'])
+      array.concat(-2['and'])
       #array.insert(-2, ['and']).join(', ')
       #array.join(', ').insert(-2, ['and'])
     #   while array == array[-2]
